75312b23f26156a4842dddbc03ba7a5f8d4b01b6
siriusonestar <siriusonestar@yandex.ru>
2019-01-28 20:53:14 +0300

    internationalization

--- src/main/java/org/controller/Hello.java
+++ src/main/java/org/controller/Hello.java
@@ -4,12 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Locale;
+
 @Controller
 
 public class Hello {
+
+
     @RequestMapping("/")
-    public String printHello(Model model){
+    public String printHello(Model model, Locale locale){
         model.addAttribute("message", "hello from controller");
+        System.out.println(locale.getDisplayLanguage());
         return "index";
     }
 
