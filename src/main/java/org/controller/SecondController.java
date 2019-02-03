da5c99563f6763fab375b71cdee5e72aecc37f7e
siriusonestar <siriusonestar@yandex.ru>
2019-02-03 12:08:29 +0300

    well

--- src/main/java/org/controller/SecondController.java
+++ src/main/java/org/controller/SecondController.java
@@ -7,21 +7,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+
 @Controller
 public class SecondController {
 
-    @RequestMapping(value = "/second",method = RequestMethod.GET)
+
+
+    @RequestMapping(value = "/second",method = RequestMethod.GET )
     public String secondPageView(Model model){
         model.addAttribute("message", "Hello from second controller ");
         return "second";
     }
 
-    @RequestMapping(value = "/third/${inputName}", method = RequestMethod.POST)
-    public String helloWithName(@PathVariable("inputName")
-                                @RequestParam(value = "inputName")
-                                String inputName, Model model){
-      model.addAttribute("messageHello", "Hello " + inputName);
+    public static String name;
 
+    @RequestMapping(value = "/third", method = RequestMethod.POST)
+    public String helloWithName(@RequestParam(value = "inputName") String inputName, Model model ){
+
+        name = inputName;
+        model.addAttribute("messageHello", "Hello " + inputName);
         return "third";
+    }
+
+    @RequestMapping(value = "/four", method = RequestMethod.GET)
+    public String getInputName(@PathVariable String inputName){
+
+
+        return "four";
     }
 }
